2a6c04dd070e800ded2e7e44178217250e8e633b
André Luciani <andrelucianidasilva@gmail.com>
2023-08-12 20:52:31 +0000

    finished prism intro examples

--- components/Prisms.vue
+++ components/Prisms.vue
@@ -19,7 +19,11 @@ import { generatePrism } from '../utils'
 
 const rot = ref<[number, number, number]>([0, 0, 0]);
 const sceneRef = ref<Ref<typeof Scene> | null>(null);
-const { vertices, faces } = generatePrism(3, 5, 7, - Math.PI / 6)
+const examples = [
+    generatePrism(3, 5, 7, Math.PI / 6),
+    generatePrism(4, 5, 7, Math.PI / 18),
+    generatePrism(4, 5, 7, 0)
+]
 
 onMounted(() => {
     if (sceneRef.value) {
@@ -32,26 +36,51 @@ onMounted(() => {
 <template>
     <div style="width: 100%; height: 100%;" border="~ main rounded-lg" overflow-hidden>
         <Renderer :antialias="true">
-            <OrthographicCamera :position="[-60, 150, 100]" :lookAt="[0, 0, 0]">
+            <OrthographicCamera :position="[-30, 30, 30]" :lookAt="[0, 0, 0]">
                 <OrbitControls />
             </OrthographicCamera>
             <Scene ref="sceneRef" background="#ededed">
                 <AmbientLight :position="[0, 4, 4]" :intensity="1" />
                 <!-- Base Plane -->
-                <Mesh :position="[0, 0, 0]" :rotation="[Math.PI / 2, 0, 0]">
+                <Mesh :position="[0, -0.1, 0]" :rotation="[Math.PI / 2, 0, 0]">
                     <MeshBasicMaterial color="#ededed" :side="DoubleSide" />
                     <PlaneGeometry :width="2000" :height="2000" />
                 </Mesh>
                 <Group>
                     <Group>
-                        <!-- Prism -->
-                        <Mesh :position="[0, 4, 0]" :rotation="rot">
+                        <!-- Oblique Triangular -->
+                        <Mesh :position="[0, 0, 0]" :rotation="rot">
                             <MeshBasicMaterial color="#118ab2" :side="DoubleSide" :transparent="true" :opacity="0.8" />
-                            <BufferGeometry name="prism" :vertices="vertices" :faces="faces" />
+                            <BufferGeometry name="oblique-triangular" :vertices="examples[0].vertices"
+                                :faces="examples[0].faces" />
                         </Mesh>
-                        <LineSegments :position="[0, 4, 0]" :rotation="rot">
+                        <LineSegments :position="[0, 0, 0]" :rotation="rot">
                             <LineBasicMaterial color="black" />
-                            <EdgesGeometry geometry="prism" />
+                            <EdgesGeometry geometry="oblique-triangular" />
+                        </LineSegments>
+                    </Group>
+                    <Group>
+                        <!-- Right Square -->
+                        <Mesh :position="[1, 0, 12]" :rotation="rot">
+                            <MeshBasicMaterial color="#118ab2" :side="DoubleSide" :transparent="true" :opacity="0.8" />
+                            <BufferGeometry name="right-square" :vertices="examples[2].vertices"
+                                :faces="examples[2].faces" />
+                        </Mesh>
+                        <LineSegments :position="[1, 0, 12]" :rotation="rot">
+                            <LineBasicMaterial color="black" />
+                            <EdgesGeometry geometry="right-square" />
+                        </LineSegments>
+                    </Group>
+                    <Group>
+                        <!-- Oblique Square -->
+                        <Mesh :position="[0, 0, 24]" :rotation="rot">
+                            <MeshBasicMaterial color="#118ab2" :side="DoubleSide" :transparent="true" :opacity="0.8" />
+                            <BufferGeometry name="oblique-square" :vertices="examples[1].vertices"
+                                :faces="examples[1].faces" />
+                        </Mesh>
+                        <LineSegments :position="[0, 0, 24]" :rotation="rot">
+                            <LineBasicMaterial color="black" />
+                            <EdgesGeometry geometry="oblique-square" />
                         </LineSegments>
                     </Group>
                 </Group>
