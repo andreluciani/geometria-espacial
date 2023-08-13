fa7bb9a31c8c0acc30db9554a3a82a388aef1728
André Luciani <andrelucianidasilva@gmail.com>
2023-08-13 23:56:26 +0000

    added top face and drafted plan animation

--- components/prism/TriangularNet.vue
+++ components/prism/TriangularNet.vue
@@ -8,9 +8,8 @@ import {
     LineSegments,
     GridHelper,
 } from "@janvorisek/drie";
-import { ref } from "vue"
+import { ref, reactive, computed } from "vue"
 import { DoubleSide } from "three";
-import { generatePrism } from "../../utils";
 
 const props = defineProps({
     position: {
@@ -29,11 +28,31 @@ const props = defineProps({
     }
 })
 
-const theta = Math.PI / 2 // -> make reactive!
+
+const currentRotation = reactive({
+    facesRotation: 0,
+    topRotation: 0
+})
+
+// const theta = Math.PI / 2 // -> make reactive!
+const theta = computed(() => (currentRotation.facesRotation))
 const thirty = Math.PI / 6
 const fourtyFive = Math.PI / 4
 const sixty = Math.PI / 3
 
+
+window.setInterval(() => {
+    const newAngle: Array<number> = [...faceRotation.value];
+    currentRotation.facesRotation += Math.PI / 360
+    if (props.plan) {
+        currentRotation.facesRotation += Math.PI / 360
+    }
+    if (props.resetPlan) {
+        currentRotation.facesRotation = 0;
+        currentRotation.topRotation = 0;
+    }
+    faceRotation.value = newAngle;
+}, 10);
 
 const triangularPrismFaces = [
     {
@@ -46,15 +65,15 @@ const triangularPrismFaces = [
         // face 1
         vertices: [-1, 0, 0, 1, 0, 0, 1, 2, 0, -1, 2, 0],
         faces: [0, 1, 2, 2, 3, 0],
-        rotation: [-theta, 0, 0]
+        rotation: [-theta.value, 0, 0]
     },
     {
         // face 2
         vertices: [
             -1, 0, -Math.sqrt(3) / 3, // 0
             0, 0, 2 * Math.sqrt(3) / 3, // 1
-            -1 - 2 * Math.sin(theta) * Math.cos(thirty), 2 * Math.cos(theta), -Math.sqrt(3) / 3 + 2 * Math.sin(theta) * Math.sin(thirty), // 2
-            -2 * Math.sin(theta) * Math.cos(thirty), 2 * Math.cos(theta), 2 * Math.sqrt(3) / 3 + 2 * Math.sin(theta) * Math.sin(thirty), //3
+            -1 - 2 * Math.sin(theta.value) * Math.cos(thirty), 2 * Math.cos(theta.value), -Math.sqrt(3) / 3 + 2 * Math.sin(theta.value) * Math.sin(thirty), // 2
+            -2 * Math.sin(theta.value) * Math.cos(thirty), 2 * Math.cos(theta.value), 2 * Math.sqrt(3) / 3 + 2 * Math.sin(theta.value) * Math.sin(thirty), //3
         ],
         faces: [0, 1, 3, 3, 2, 0],
         rotation: [0, 0, 0]
@@ -64,33 +83,21 @@ const triangularPrismFaces = [
         vertices: [
             1, 0, -Math.sqrt(3) / 3, // 0
             0, 0, 2 * Math.sqrt(3) / 3, // 1
-            1 + 2 * Math.sin(theta) * Math.cos(thirty), 2 * Math.cos(theta), -Math.sqrt(3) / 3 + 2 * Math.sin(theta) * Math.sin(thirty), // 2
-            2 * Math.sin(theta) * Math.cos(thirty), 2 * Math.cos(theta), 2 * Math.sqrt(3) / 3 + 2 * Math.sin(theta) * Math.sin(thirty), // 3
+            1 + 2 * Math.sin(theta.value) * Math.cos(thirty), 2 * Math.cos(theta.value), -Math.sqrt(3) / 3 + 2 * Math.sin(theta.value) * Math.sin(thirty), // 2
+            2 * Math.sin(theta.value) * Math.cos(thirty), 2 * Math.cos(theta.value), 2 * Math.sqrt(3) / 3 + 2 * Math.sin(theta.value) * Math.sin(thirty), // 3
         ],
         faces: [0, 1, 3, 3, 2, 0],
         rotation: [0, 0, 0]
     },
     {
-        vertices: [0, 0, 0],
-        faces: [0, 0, 0],
+        // top
+        vertices: [-1, 2, -Math.sqrt(3) / 3, 1, 2, -Math.sqrt(3) / 3, 0, 2, 2 * Math.sqrt(3) / 3],
+        faces: [0, 1, 2],
         rotation: [0, 0, 0]
-    }, // top
+    },
 ]
 const faceRotation = ref<Array<number>>([0, 0, 0]);
 const topRotation = ref<Array<number>>([0, 0, 0]);
-
-window.setInterval(() => {
-    const newAngle: Array<number> = [...faceRotation.value];
-    if (props.plan) {
-        newAngle[0] = newAngle[0] - 0.002
-    }
-    if (props.resetPlan) {
-        newAngle[0] = 0
-        newAngle[1] = 0
-        newAngle[2] = 0
-    }
-    faceRotation.value = newAngle;
-}, 10);
 
 </script>
 
@@ -135,6 +142,16 @@ window.setInterval(() => {
     <LineSegments :position="[0, 0, 0]" :rotation="triangularPrismFaces[3].rotation">
         <LineBasicMaterial color="black" />
         <EdgesGeometry geometry="right-triangular-face-3" />
+    </LineSegments>
+    <!-- Right Triangular - Top -->
+    <Mesh :position="[0, 0, 0]" :rotation="triangularPrismFaces[4].rotation">
+        <MeshBasicMaterial color="#118ab2" :side="DoubleSide" :transparent="true" :opacity="0.8" />
+        <BufferGeometry name="right-triangular-top" :vertices="triangularPrismFaces[4].vertices"
+            :faces="triangularPrismFaces[4].faces" />
+    </Mesh>
+    <LineSegments :position="[0, 0, 0]" :rotation="triangularPrismFaces[4].rotation">
+        <LineBasicMaterial color="black" />
+        <EdgesGeometry geometry="right-triangular-top" />
     </LineSegments>
 </template>
 
