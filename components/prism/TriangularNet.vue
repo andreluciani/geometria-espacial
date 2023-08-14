d50bb0a5436c95cb800b415eb5509ddf326d2f71
André Luciani <andrelucianidasilva@gmail.com>
2023-08-14 01:11:32 +0000

    triangular prism net animation

--- components/prism/TriangularNet.vue
+++ components/prism/TriangularNet.vue
@@ -39,6 +39,7 @@ const props = defineProps({
 })
 
 const facesRotation = ref<number>(props.facesRotation);
+const topRotation = ref<number>(props.topRotation);
 const thirty = Math.PI / 6
 
 const initialSetup = [
@@ -88,15 +89,25 @@ const faces = ref<Array<{ vertices: Array<number>, faces: Array<number>, rotatio
 
 
 window.setInterval(() => {
-    let newAngle = facesRotation.value + Math.PI / 100
+    if (!props.plan) {
+        return
+    }
+    let newAngle = facesRotation.value + Math.PI / 200
+    let newTopAngle = Math.min(topRotation.value + Math.PI / 100, Math.PI / 2)
     if (props.resetPlan) {
         newAngle = 0
+        newTopAngle = 0
+        facesRotation.value = Math.min(newAngle, Math.PI / 2)
+        topRotation.value = Math.min(newTopAngle, Math.PI / 2)
     }
     facesRotation.value = Math.min(newAngle, Math.PI / 2)
+    if (facesRotation.value === Math.PI / 2) {
+        topRotation.value = Math.min(newTopAngle, Math.PI / 2)
+    }
     const newSetup = [
         {
             // base
-            vertices: [-1, 0, -Math.sqrt(3) / 3, 1, 0, -Math.sqrt(3) / 3, 0, 0, 2 * Math.sqrt(3) / 3],
+            vertices: [-1, 0, 0, 1, 0, 0, 0, 0, 3 * Math.sqrt(3) / 3],
             faces: [0, 1, 2],
             rotation: [0, 0, 0]
         },
@@ -109,10 +120,10 @@ window.setInterval(() => {
         {
             // face 2
             vertices: [
-                -1, 0, -Math.sqrt(3) / 3, // 0
-                0, 0, 2 * Math.sqrt(3) / 3, // 1
-                -1 - 2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), -Math.sqrt(3) / 3 + 2 * Math.sin(newAngle) * Math.sin(thirty), // 2
-                -2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), 2 * Math.sqrt(3) / 3 + 2 * Math.sin(newAngle) * Math.sin(thirty), //3
+                -1, 0, 0, // 0
+                0, 0, 3 * Math.sqrt(3) / 3, // 1
+                -1 - 2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), 2 * Math.sin(newAngle) * Math.sin(thirty), // 2
+                -2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), 3 * Math.sqrt(3) / 3 + 2 * Math.sin(newAngle) * Math.sin(thirty), //3
             ],
             faces: [0, 1, 3, 3, 2, 0],
             rotation: [0, 0, 0]
@@ -120,23 +131,26 @@ window.setInterval(() => {
         {
             // face 3
             vertices: [
-                1, 0, -Math.sqrt(3) / 3, // 0
-                0, 0, 2 * Math.sqrt(3) / 3, // 1
-                1 + 2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), -Math.sqrt(3) / 3 + 2 * Math.sin(newAngle) * Math.sin(thirty), // 2
-                2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), 2 * Math.sqrt(3) / 3 + 2 * Math.sin(newAngle) * Math.sin(thirty), // 3
+                1, 0, 0, // 0
+                0, 0, 3 * Math.sqrt(3) / 3, // 1
+                1 + 2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), 2 * Math.sin(newAngle) * Math.sin(thirty), // 2
+                2 * Math.sin(newAngle) * Math.cos(thirty), 2 * Math.cos(newAngle), 3 * Math.sqrt(3) / 3 + 2 * Math.sin(newAngle) * Math.sin(thirty), // 3
             ],
             faces: [0, 1, 3, 3, 2, 0],
             rotation: [0, 0, 0]
         },
         {
             // top
-            vertices: [-1, 2, -Math.sqrt(3) / 3, 1, 2, -Math.sqrt(3) / 3, 0, 2, 2 * Math.sqrt(3) / 3],
+            vertices: [
+                -1, 2, 0, // 0
+                1, 2, 0, // 1
+                0, 2 + 3 * Math.sqrt(3) / 3 * Math.sin(newTopAngle), 3 * Math.sqrt(3) / 3 * Math.cos(newTopAngle)],
             faces: [0, 1, 2],
             rotation: [-newAngle, 0, 0]
         },
     ]
     faces.value = newSetup
-}, 100);
+}, 50);
 
 </script>
 
@@ -151,11 +165,11 @@ window.setInterval(() => {
         <EdgesGeometry geometry="right-triangular-base" />
     </LineSegments>
     <!-- Right Triangular - face 1 -->
-    <Mesh :position="[0, 0, -(Math.sqrt(3) / 3)]" :rotation="faces[1].rotation">
+    <Mesh :position="[0, 0, 0]" :rotation="faces[1].rotation">
         <MeshBasicMaterial color="#118ab2" :side="DoubleSide" :transparent="true" :opacity="0.8" />
         <BufferGeometry name="right-triangular-face-1" :vertices="faces[1].vertices" :faces="faces[1].faces" />
     </Mesh>
-    <LineSegments :position="[0, 0, -(Math.sqrt(3) / 3)]" :rotation="faces[1].rotation">
+    <LineSegments :position="[0, 0, 0]" :rotation="faces[1].rotation">
         <LineBasicMaterial color="black" />
         <EdgesGeometry geometry="right-triangular-face-1" />
     </LineSegments>
